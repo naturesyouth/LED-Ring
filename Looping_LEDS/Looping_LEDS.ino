81bfb8414a3063bfa243b80a9546c84277743ae6
William Hogg <naturesyouth@gmail.com>
2015-11-28 17:36:09 +0000

    updated with a new set of serial read calls

--- Looping_LEDS/Looping_LEDS.ino
+++ Looping_LEDS/Looping_LEDS.ino
@@ -4,7 +4,7 @@
 #define num_leds 24
 
 WS2812 LED(num_leds);
-cRGB value; 
+cRGB value;
 byte intensity[4];
 byte sign;
 int offset = 0;
@@ -15,48 +15,48 @@ void setup() {
   LED.setOutput(output_pin);
   sign = 1;
   Serial.println("starting");
-  set_all_LED(32,0,0);
+  set_all_LED(32, 0, 0);
   delay(100);
-    set_all_LED(0,32,0);
+  set_all_LED(0, 32, 0);
   delay(100);
-    set_all_LED(0,0,32);
+  set_all_LED(0, 0, 32);
   delay(100);
-    set_all_LED(0,0,0);
+  set_all_LED(0, 0, 0);
   delay(10);
 
 }
 
 void loop() {
 
+  while(!Serial.available());
 
-  byte serialred = Serial.read();
+  byte serialred = Serial.read() -48;
+  byte serialgreen = Serial.read()-48;
+  byte serialblue = Serial.read()-48;
+
   Serial.println(serialred);
-   while(!Serial.available());
-  byte serialgreen = Serial.read();
-   while(!Serial.available());
   Serial.println(serialgreen);
-  byte serialblue = Serial.read();
   Serial.println(serialblue);
 
 
   Serial.println("setting leds");
-  set_all_LED(serialred,serialgreen,serialblue);
-  
-   if(offset<num_leds){
+  set_all_LED(serialred, serialgreen, serialblue);
+
+  if (offset < num_leds) {
     offset++;
-  }else{
-    offset=0;
+  } else {
+    offset = 0;
   }
-delay(10);
-  }
-  
-  void set_all_LED(byte r, byte b, byte g){
+  delay(100);
+}
+
+void set_all_LED(byte r, byte b, byte g) {
   for (int i = 0; i < num_leds; i++) {
     value.r = r;
     value.g = g;
     value.b = b;
     LED.set_crgb_at(i, value);
   }
-    LED.sync();
+  LED.sync();
 }
 
